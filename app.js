40a6a12bdb26fdf6a58432f1b194d147ffb87dfe
Kelvin Pires <pireskelvin7@gmail.com>
2021-09-26 00:35:27 -0300

    :sparkles: Finish App

---
+++ app.js
@@ -0,0 +1,27 @@
+const root = document.querySelector("#root");
+const arrows = document.querySelectorAll(".arrow");
+
+const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+let i = 0;
+
+root.style.background = `url(./images/${images[0]})`;
+root.style.borderRadius = ".8rem";
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", (e) => {
+    if (e.target.id == "left") {
+      i--;
+      if (i < 0) {
+        i = images.length - 1;
+      }
+      root.style.background = `url(./images/${images[i]})`;
+    }
+    if (e.target.id == "right") {
+      i++;
+      if (i > images.length - 1) {
+        i = 0;
+      }
+      root.style.background = `url(./images/${images[i]})`;
+    }
+  });
+});
